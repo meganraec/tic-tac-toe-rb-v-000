4a28aa3ddaa79046046228e3dfe6d8c325b04640
Megan Collier <meganraec@live.com>
2018-01-30 23:11:37 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -50,7 +50,7 @@ def turn(board)
     input = gets.strip
     index = input_to_index(input)
   end
-  puts move(board, index, character)
+  puts move(board, index, '#{current_player(board)}')
   puts display(board)
 end
 
